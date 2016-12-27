45a14b9e20a85cf70718768a51032d5f27ff266b
RobinDHBW <thyeria@gmx.de>
2016-12-27 16:52:45 +0100

    Revert "Header View Klasse implementiert"
    This reverts commit 5388802f1db00e79de90aa39bf59c94da450af95.

--- src/main/java/com/dhbwProject/views/HeaderView.java
+++ src/main/java/com/dhbwProject/views/HeaderView.java
@@ -1,25 +1,5 @@
 package com.dhbwProject.views;
 
-import com.vaadin.server.ThemeResource;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Image;
-import com.vaadin.ui.Label;
-
 public class HeaderView {
-	public HeaderView()
-	{
-	HorizontalLayout hL = new HorizontalLayout();
- 
-	Image dhbwLogo = new Image(null,
-		    new ThemeResource("Logo DHBW_Mosbach.gif"));
-		hL.addComponent(dhbwLogo);
-	
-	Label whiteL = new Label(" ");
-	Button logoutB = new Button();
-	logoutB.setCaption("Abmelden");
- 
-	hL.addComponent(whiteL);
-	hL.addComponent(logoutB);
-	}
+
 }
