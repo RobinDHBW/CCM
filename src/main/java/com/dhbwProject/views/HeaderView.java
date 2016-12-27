5388802f1db00e79de90aa39bf59c94da450af95
JasminDHBW <j.stribik@gmx.de>
2016-12-27 12:21:08 +0100

    Header View Klasse implementiert

--- src/main/java/com/dhbwProject/views/HeaderView.java
+++ src/main/java/com/dhbwProject/views/HeaderView.java
@@ -1,5 +1,25 @@
 package com.dhbwProject.views;
 
-public class HeaderView {
+import com.vaadin.server.ThemeResource;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Image;
+import com.vaadin.ui.Label;
 
+public class HeaderView {
+	public HeaderView()
+	{
+	HorizontalLayout hL = new HorizontalLayout();
+ 
+	Image dhbwLogo = new Image(null,
+		    new ThemeResource("Logo DHBW_Mosbach.gif"));
+		hL.addComponent(dhbwLogo);
+	
+	Label whiteL = new Label(" ");
+	Button logoutB = new Button();
+	logoutB.setCaption("Abmelden");
+ 
+	hL.addComponent(whiteL);
+	hL.addComponent(logoutB);
+	}
 }
