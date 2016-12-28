b4384aa1aa8eaf778f103e0245be0ab3583f3f4b
DevRealHard <elquapppo@googlemail.com>
2016-12-28 15:33:36 +0100

    Beans implementiert
    Ansprechpartner + Rolle + Gesprächsnotiz + Adresse implementiert

--- src/main/java/com/dhbwProject/backend/beans/Rolle.java
+++ src/main/java/com/dhbwProject/backend/beans/Rolle.java
@@ -1,10 +1,27 @@
 package com.dhbwProject.backend.beans;
 
 public class Rolle {
-
-	public Rolle() {
-		// TODO Auto-generated constructor stub
+	
+	public Rolle(StringBuffer berechtigung, String bezeichnung, int id) {
+		super();
+		this.berechtigung = berechtigung;
+		this.bezeichnung = bezeichnung;
+		this.id = id;
 	}
+	private StringBuffer berechtigung;
+	private String bezeichnung;
+	private int id;
+	public StringBuffer getBerechtigung() {
+		return berechtigung;
+	}
+	public String getBezeichnung() {
+		return bezeichnung;
+	}
+	public int getId() {
+		return id;
+	}
+
+
 
 	//folgende Tabellen der DB müssenn noch im Bean implementiert werden:
 	//rolle, berechtigung
